8ba1e4e4c1ed1dabbde93ab8bb74d7c40b5b129a
Matteo Martellini <git@mercxry.me>
2021-07-14 13:57:30 +0200

    init

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,33 @@
+# macOS aliasses
+if [[ $OSTYPE == darwin* ]]; then
+  alias flush="sudo killall -HUP mDNSResponder"
+fi
+
+# zsh aliases
+alias zshrc="$EDITOR ~/.zshrc"
+alias zshconfig="$EDITOR ~/.zshrc"
+alias ohmyzsh="$EDITOR ~/.oh-my-zsh"
+alias reload="source ~/.zshrc"
+alias r="source ~/.zshrc"
+
+# ls aliases
+alias ls="exa"
+alias la="ls -la"
+
+# cd shortcuts
+alias projects="cd ~/Documents/Projects"
+
+# docker aliases
+alias d="docker"
+
+# kubectl aliases
+alias k="kubectl"
+alias kgx="kubectl config get-contexts"
+
+# networking aliases
+alias myip="curl http://ipecho.net/plain; echo"
+
+# javascript aliases
+alias y="yarn"
+alias rnm="rm -rf node_modules"
+
