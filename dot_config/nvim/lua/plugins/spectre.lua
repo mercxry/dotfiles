fbcc51b1db75a39209b730275e9cfc3fa6006aec
Matteo Martellini <git@mercxry.me>
2023-07-06 14:28:46 +0200

    refactor: remove packer files

--- dot_config/nvim/lua/plugins/spectre.lua
+++
@@ -1,4 +0,0 @@
-require('spectre').setup()
-
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap('n', '<leader>S', '<cmd>lua require(\'spectre\').open()<CR>', opts)
--- dot_config/nvim/lua/plugins/tree.lua
+++
@@ -1,9 +0,0 @@
-require 'nvim-tree'.setup {
-    actions = {
-        open_file = {
-            quit_on_open = 1
-        }
-    }
-}
-
-vim.cmd [[nnoremap <C-n> :NvimTreeToggle<CR>]]
