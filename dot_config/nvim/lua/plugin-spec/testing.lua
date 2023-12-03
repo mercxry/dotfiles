53b09719131f42e386fce4c5322bb80982746808
Matteo Martellini <git@mercxry.me>
2023-12-03 21:58:56 +0100

    fix(neovim): neotest config

--- dot_config/nvim/lua/plugin-spec/testing.lua
+++ dot_config/nvim/lua/plugin-spec/testing.lua
@@ -1,19 +1,23 @@
 return {
     {
         "nvim-neotest/neotest",
+        keys = { "<leader>n" },
         dependencies = {
             "nvim-lua/plenary.nvim",
             "antoinemadec/FixCursorHold.nvim",
             "rouge8/neotest-rust",
             "nvim-neotest/neotest-go",
         },
-        opts = {
-            adapters = {
-                require("neotest-rust") {
-                    args = { "--no-capture" },
-                },
-                require("neotest-go"),
-            }
-        }
+        config = function()
+            local neotest = require("neotest")
+            neotest.setup({
+                adapters = {
+                    require("neotest-rust") {
+                        args = { "--no-capture" },
+                    },
+                    require("neotest-go"),
+                }
+            })
+        end,
     }
 }
